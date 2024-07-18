dc7a9f84056318b9df50494dba5f805d02db23e8
chutiya-2003 <gimchudiya@gmail.com>
2024-07-19 01:36:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,VAJIRA-MD=yYVhVAYa#YQGnf_TzXHaIsK03rCyKEn2KJDSGFrj05WZ_gvMO9_8
+SESSION_ID: process.env.SESSION_ID || 'youre session id' ,VAJIRA-MD=fN1hVTBa#A5b7Jh1biHPRXHrzOE3shnMTxjUVk3sajIU6foHM89s
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
